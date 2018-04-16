478509253376f303d53076152357922cf264baf7
Reka Norman <rekanorman@gmail.com>
2018-04-16 22:52:38 +1200

    bug fixes

--- src/Game.java
+++ src/Game.java
@@ -647,14 +647,7 @@ public class Game {
 					System.out.println(String.format("Your team has been rewarded with %s coins.\n",
 														PRIZE_MONEY));
 				}
-			}
-		
-				if (villain.isDefeated()) {
-					team.setCurrentMoney(team.getCurrentMoney() + PRIZE_MONEY);
-					System.out.println(String.format("Your team has been rewarded with %s coins.\n",
-														PRIZE_MONEY));
-				}
-			else {
+			} else {
 				int damage = villain.getDamageDealt();
 				team.takeDamage(hero, damage);
 				
