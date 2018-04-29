2fb2748f7326b8d3b3613d0dd0aa4778030140c0
Nathan Cleaver <akechta@gmail.com>
2018-04-29 21:58:27 +1200

    Update HomeBasePanel

--- src/HomeBasePanel.java
+++ src/HomeBasePanel.java
@@ -39,19 +39,14 @@ public class HomeBasePanel extends JPanel {
 	 * A string representation of the status panel, used by the cardlayout of the
 	 * main content panel.
 	 */
-	public static final String STATUS_PANEL = "Status Panel";
+	public static final String STATUS_PANEL_STRING = "Status Panel";
 	
 	/**
 	 * The main game that this panel is a part of. Used by event handlers
 	 * to change the panel shown in game.
 	 */
 	private Game gameWindow;
-	
-	/**
-	 * The team playing through the current game.
-	 */
-	private Team team;
-	
+			
 	/**
 	 * The main content panel for the home base.
 	 */
@@ -61,22 +56,12 @@ public class HomeBasePanel extends JPanel {
 	 * A panel component of contentPanel
 	 */
 	private JPanel mapPanel;
-	
-	/**
-	 * A string description of mapPanel, used by contentPanelCardLayout.
-	 */
-	private final String MAP_PANEL_NAME = "Map Panel";
-	
+		
 	/**
 	 * A panel component of contentPanel
 	 */
 	private JPanel statusPanel;
-	
-	/**
-	 * A string description of statusPanel, used by contentPanelCardLayout.
-	 */
-	private final String STATUS_PANEL_NAME = "Status Panel";
-	
+		
 	/**
 	 * The side panel which holds buttons to change between views in the main
 	 * content panel, and other information.
@@ -108,11 +93,9 @@ public class HomeBasePanel extends JPanel {
 	 */
 	private JButton btnUseMap;
 		
-		
 	public HomeBasePanel(Game game) {
 		super();
 		this.gameWindow = game;
-		this.team = game.getGame().getTeam();
 		
 		setLayout(null);
 		setPreferredSize(new Dimension(880, 610));
@@ -136,11 +119,11 @@ public class HomeBasePanel extends JPanel {
 	
 	private void addSidePanel() {
 		sidePanel = new JPanel();
-		sidePanel.setBounds(0, 75, 190, 535);
+		sidePanel.setBounds(10, 86, 215, 513);
 		sidePanel.setLayout(null);
 		
 		JLabel lblCurrentCity = new JLabel();
-		lblCurrentCity.setBounds(65, 11, 52, 25);
+		lblCurrentCity.setBounds(77, 11, 52, 25);
 		lblCurrentCity.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		lblCurrentCity.setHorizontalAlignment(SwingConstants.CENTER);
 		lblCurrentCity.setText("City X");
@@ -149,52 +132,55 @@ public class HomeBasePanel extends JPanel {
 		btnDisplayCity = new JButton("Display City");
 		btnDisplayCity.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				contentPanelCardLayout.show(contentPanel, MAP_PANEL_NAME);
+				contentPanelCardLayout.show(contentPanel, MAP_PANEL_STRING);
 				btnDisplayCity.setEnabled(false);
 				btnViewStatus.setEnabled(true);
+				refreshMapPanel();
 			}
 		});
-		btnDisplayCity.setBounds(36, 97, 120, 25);
+		btnDisplayCity.setBounds(46, 64, 120, 25);
 		sidePanel.add(btnDisplayCity);
 		
 		btnViewStatus = new JButton("View Team Status");
 		btnViewStatus.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				contentPanelCardLayout.show(contentPanel, STATUS_PANEL_NAME);
+				contentPanelCardLayout.show(contentPanel, STATUS_PANEL_STRING);
 				btnViewStatus.setEnabled(false);
 				btnDisplayCity.setEnabled(true);
+				refreshStatusPanel();
 			}
 		});
-		btnViewStatus.setBounds(36, 157, 120, 25);
+		btnViewStatus.setBounds(46, 124, 120, 25);
 		sidePanel.add(btnViewStatus);
 		
 		btnUseMap = new JButton("Use Map");
 		btnUseMap.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				gameWindow.getGame().getCurrentCity().setAllDiscovered();
-				team.setNumMaps(team.getNumMaps() - 1);
+				getTeam().setNumMaps(getTeam().getNumMaps() - 1);
+				contentPanelCardLayout.show(contentPanel, MAP_PANEL_STRING);
 			}
 		});
-		btnUseMap.setBounds(36, 217, 120, 25);
-		if (team.getNumMaps() <= 0) {
+		btnUseMap.setBounds(46, 184, 120, 25);
+		if (getTeam().getNumMaps() <= 0) {
 			btnUseMap.setEnabled(false);
 		}
 		sidePanel.add(btnUseMap);
 		
-		JLabel lblOwned = new JLabel(String.format("Owned: %d", team.getNumMaps()));
+		JLabel lblOwned = new JLabel(String.format("Owned: %d", getTeam().getNumMaps()));
 		lblOwned.setHorizontalAlignment(SwingConstants.CENTER);
-		lblOwned.setBounds(36, 242, 120, 14);
+		lblOwned.setBounds(46, 207, 120, 14);
 		sidePanel.add(lblOwned);
 				
 		JTextPane txtRandomEvent = new JTextPane();
 		txtRandomEvent.setBackground(new Color(240, 240, 240));
 		txtRandomEvent.setEditable(false);
 		txtRandomEvent.setText("Sample Text\r\nThe villagers aid you in your fight againts the villains!\r\nYou received: Alicorn Dust");
-		txtRandomEvent.setBounds(10, 340, 170, 97);
+		txtRandomEvent.setBounds(10, 292, 170, 97);
 		sidePanel.add(txtRandomEvent);
 
 		JLabel lblRandomEvent = new JLabel("Random Event!");
-		lblRandomEvent.setBounds(10, 326, 82, 14);
+		lblRandomEvent.setBounds(10, 279, 82, 14);
 		sidePanel.add(lblRandomEvent);
 	
 		add(sidePanel);	
@@ -202,14 +188,22 @@ public class HomeBasePanel extends JPanel {
 	
 	private void addContentPanel() {
 		contentPanel = new JPanel();
-		contentPanel.setBounds(190, 75, 690, 535);
+		contentPanel.setBounds(235, 86, 632, 513);
 		contentPanelCardLayout = new CardLayout(0, 0);
 		contentPanel.setLayout(contentPanelCardLayout);
-
-		Hero hero1 = team.getHeroes().get(0);
 				
+		addMapPanel();		
+		addStatusPanel();
+		
+		contentPanelCardLayout.show(contentPanel, MAP_PANEL_STRING);
+		btnDisplayCity.setEnabled(false);
+		add(contentPanel);
+	}
+	
+	private void addMapPanel() {
+
 		mapPanel = new JPanel();
-		contentPanel.add(mapPanel, MAP_PANEL_NAME);
+		contentPanel.add(mapPanel, MAP_PANEL_STRING);
 		mapPanel.setLayout(new GridLayout(3, 3, 0, 0));
 		
 		JLabel lblNorthWest = new JLabel("");
@@ -247,173 +241,84 @@ public class HomeBasePanel extends JPanel {
 		JLabel lblSouthEast = new JLabel("");
 		lblSouthEast.setIcon(new ImageIcon(HomeBasePanel.class.getResource("/img/mountains.png")));
 		mapPanel.add(lblSouthEast);
-		
+	}
+	
+	private void addStatusPanel() {		
 		statusPanel = new JPanel();
-		contentPanel.add(statusPanel, STATUS_PANEL_NAME);
+		contentPanel.add(statusPanel, STATUS_PANEL_STRING);
 		statusPanel.setLayout(new GridLayout(0, 3, 0, 0));
+	}
+	
+	private void refreshMapPanel() {
 		
-		JPanel pnlHero1Status = new JPanel();
-		statusPanel.add(pnlHero1Status);
-		pnlHero1Status.setLayout(null);
+	}
+	
+	/**
+	 * Updates variable fields in the statusPanel
+	 * Called whenever the statusPanel is displayed
+	 */
+	private void refreshStatusPanel() {
+		statusPanel.removeAll();
 		
-		JLabel imgHero1Portrait = new JLabel("");
-		imgHero1Portrait.setBounds(33, 11, 150, 150);
-		imgHero1Portrait.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-		imgHero1Portrait.setIcon(new ImageIcon(HomeBasePanel.class.getResource("/img/bulwark_portrait.png"))); //update this to 150x150 version
-		pnlHero1Status.add(imgHero1Portrait);
-		
-		JLabel lblHero1Name = new JLabel(String.format("%s the %s", hero1.getName(),
-																	hero1.getType()));
-		lblHero1Name.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		lblHero1Name.setHorizontalAlignment(SwingConstants.CENTER);
-		lblHero1Name.setBounds(10, 164, 199, 32);
-		pnlHero1Status.add(lblHero1Name);
-		
-		JLabel lblHero1Ability = new JLabel(String.format("%s", hero1.getSpecialAbility()));
-		lblHero1Ability.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		lblHero1Ability.setHorizontalAlignment(SwingConstants.CENTER);
-		lblHero1Ability.setBounds(20, 203, 181, 53);
-		pnlHero1Status.add(lblHero1Ability);
-		
-		JLabel lblHero1Health = new JLabel(String.format("Health: %d/%d", hero1.getCurrentHealth(), hero1.getMaxHealth()));
-		lblHero1Health.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		lblHero1Health.setHorizontalAlignment(SwingConstants.CENTER);
-		lblHero1Health.setBounds(10, 267, 199, 44);
-		pnlHero1Status.add(lblHero1Health);
-		
-		JLabel lblHero1HealingItem = new JLabel(String.format("Healing Item:\r\n%s", hero1.getAppliedHealingItem().getName()));
-		lblHero1HealingItem.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		lblHero1HealingItem.setHorizontalAlignment(SwingConstants.CENTER);
-		lblHero1HealingItem.setBounds(33, 318, 165, 32);
-		pnlHero1Status.add(lblHero1HealingItem);
-		
-		JLabel lblHero1PowerUp = new JLabel("Power Ups:");
-		lblHero1PowerUp.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		lblHero1PowerUp.setHorizontalAlignment(SwingConstants.CENTER);
-		lblHero1PowerUp.setBounds(10, 361, 210, 32);
-		pnlHero1Status.add(lblHero1PowerUp);
-		
-		JTextPane txtHero1PowerUps = new JTextPane();
-		txtHero1PowerUps.setBackground(new Color(240, 240, 240));
-		for (PowerUp pu : hero1.getActivePowerUps()) {
-			txtHero1PowerUps.setText(txtHero1PowerUps.getText() + String.format("\n%s", pu.getType().toString()));
-		}
-		txtHero1PowerUps.setEditable(false);
-		txtHero1PowerUps.setBounds(10, 402, 210, 122);
-		pnlHero1Status.add(txtHero1PowerUps);
-		
-		if (team.getHeroes().size() >= 2) {
+		for (Hero hero : getTeam().getHeroes()) {
+			JPanel heroPanel = new JPanel();
+			statusPanel.add(heroPanel);
+			heroPanel.setLayout(null);
 			
-			Hero hero2 = team.getHeroes().get(1);
+			JLabel imgHeroPortrait = new JLabel("");
+			imgHeroPortrait.setBounds(33, 24, 165, 134);
+			imgHeroPortrait.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+			heroPanel.add(imgHeroPortrait);
+
+			JLabel lblHeroName = new JLabel(String.format("%s the %s", hero.getName(),
+																	   hero.getType()));
+			lblHeroName.setFont(new Font("Tahoma", Font.PLAIN, 13));
+			lblHeroName.setHorizontalAlignment(SwingConstants.CENTER);
+			lblHeroName.setBounds(10, 164, 199, 32);
+			heroPanel.add(lblHeroName);
+
+			JLabel lblHeroAbility = new JLabel(String.format("%s", hero.getSpecialAbility()));
+			lblHeroAbility.setFont(new Font("Tahoma", Font.PLAIN, 13));
+			lblHeroAbility.setHorizontalAlignment(SwingConstants.CENTER);
+			lblHeroAbility.setBounds(20, 203, 181, 53);
+			heroPanel.add(lblHeroAbility);
+
+			JLabel lblHeroHealth = new JLabel(String.format("Health: %d/%d", hero.getCurrentHealth(), hero.getMaxHealth()));
+			lblHeroHealth.setFont(new Font("Tahoma", Font.PLAIN, 13));
+			lblHeroHealth.setHorizontalAlignment(SwingConstants.CENTER);
+			lblHeroHealth.setBounds(10, 267, 199, 44);
+			heroPanel.add(lblHeroHealth);
 			
-			JPanel pnlHero2Status = new JPanel();
-			statusPanel.add(pnlHero2Status);
-			pnlHero2Status.setLayout(null);
-			
-			JLabel imgHero2Portrait = new JLabel("");
-			imgHero2Portrait.setBounds(33, 24, 165, 134);
-			imgHero2Portrait.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-			pnlHero2Status.add(imgHero2Portrait);
-			
-			JLabel lblHero2Name = new JLabel(String.format("%s the %s", hero2.getName(),
-																		hero2.getType()));
-			lblHero2Name.setFont(new Font("Tahoma", Font.PLAIN, 13));
-			lblHero2Name.setHorizontalAlignment(SwingConstants.CENTER);
-			lblHero2Name.setBounds(10, 164, 199, 32);
-			pnlHero2Status.add(lblHero2Name);
-			
-			JLabel lblHero2Ability = new JLabel(String.format("%s", hero2.getSpecialAbility()));
-			lblHero2Ability.setFont(new Font("Tahoma", Font.PLAIN, 13));
-			lblHero2Ability.setHorizontalAlignment(SwingConstants.CENTER);
-			lblHero2Ability.setBounds(20, 203, 181, 53);
-			pnlHero2Status.add(lblHero2Ability);
-			
-			JLabel lblHero2Health = new JLabel(String.format("Health: %d/%d", hero2.getCurrentHealth(), hero2.getMaxHealth()));
-			lblHero2Health.setFont(new Font("Tahoma", Font.PLAIN, 13));
-			lblHero2Health.setHorizontalAlignment(SwingConstants.CENTER);
-			lblHero2Health.setBounds(10, 267, 199, 44);
-			pnlHero2Status.add(lblHero2Health);
-			
-			JLabel lblHero2HealingItem = new JLabel(String.format("Healing Item:\r\n%s", hero2.getAppliedHealingItem().getName()));
-			lblHero2HealingItem.setFont(new Font("Tahoma", Font.PLAIN, 13));
-			lblHero2HealingItem.setHorizontalAlignment(SwingConstants.CENTER);
-			lblHero2HealingItem.setBounds(33, 318, 165, 32);
-			pnlHero2Status.add(lblHero2HealingItem);
-			
-			JLabel lblHero2PowerUp = new JLabel("Power Ups:");
-			lblHero2PowerUp.setFont(new Font("Tahoma", Font.PLAIN, 13));
-			lblHero2PowerUp.setHorizontalAlignment(SwingConstants.CENTER);
-			lblHero2PowerUp.setBounds(10, 361, 210, 32);
-			pnlHero2Status.add(lblHero2PowerUp);
-			
-			JTextPane txtHero2PowerUps = new JTextPane();
-			txtHero2PowerUps.setBackground(new Color(240, 240, 240));
-			for (PowerUp pu : hero2.getActivePowerUps()) {
-				txtHero2PowerUps.setText(txtHero2PowerUps.getText() + String.format("\n%s", pu.getType().toString()));
+			JLabel lblHeroHealingItem = new JLabel(String.format("Healing Item:\r\n%s", hero.getAppliedHealingItem().toString()));
+			lblHeroHealingItem.setFont(new Font("Tahoma", Font.PLAIN, 13));
+			lblHeroHealingItem.setHorizontalAlignment(SwingConstants.CENTER);
+			lblHeroHealingItem.setBounds(33, 318, 165, 32);
+			heroPanel.add(lblHeroHealingItem);
+
+			JLabel lblHeroPowerUp = new JLabel("Power Ups:");
+			lblHeroPowerUp.setFont(new Font("Tahoma", Font.PLAIN, 13));
+			lblHeroPowerUp.setHorizontalAlignment(SwingConstants.CENTER);
+			lblHeroPowerUp.setBounds(10, 361, 210, 32);
+			heroPanel.add(lblHeroPowerUp);
+
+			JTextPane txtHeroPowerUps = new JTextPane();
+			txtHeroPowerUps.setBackground(new Color(240, 240, 240));
+			for (PowerUp pu : hero.getActivePowerUps()) {
+				txtHeroPowerUps.setText(txtHeroPowerUps.getText() + String.format("\n%s", pu.getType().toString()));
 			}
-			txtHero2PowerUps.setEditable(false);
-			txtHero2PowerUps.setBounds(10, 402, 210, 122);
-			pnlHero2Status.add(txtHero2PowerUps);
-			
+			txtHeroPowerUps.setEditable(false);
+			txtHeroPowerUps.setBounds(10, 402, 210, 122);
+			heroPanel.add(txtHeroPowerUps);
+
 		}
 		
-		if (team.getHeroes().size() >= 3) {
-			
-			Hero hero3 = team.getHeroes().get(2);
-			
-			JPanel pnlHero3Status = new JPanel();
-			statusPanel.add(pnlHero3Status);
-			pnlHero3Status.setLayout(null);
-			
-			JLabel imgHero3Portrait = new JLabel("");
-			imgHero3Portrait.setBounds(33, 24, 165, 134);
-			imgHero3Portrait.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-			pnlHero3Status.add(imgHero3Portrait);
-			
-			JLabel lblHero3Name = new JLabel(String.format("%s the %s", hero3.getName(),
-																		hero3.getType()));
-			lblHero3Name.setFont(new Font("Tahoma", Font.PLAIN, 13));
-			lblHero3Name.setHorizontalAlignment(SwingConstants.CENTER);
-			lblHero3Name.setBounds(10, 164, 199, 32);
-			pnlHero3Status.add(lblHero3Name);
-			
-			JLabel lblHero3Ability = new JLabel(String.format("%s", hero3.getSpecialAbility()));
-			lblHero3Ability.setFont(new Font("Tahoma", Font.PLAIN, 13));
-			lblHero3Ability.setHorizontalAlignment(SwingConstants.CENTER);
-			lblHero3Ability.setBounds(20, 203, 181, 53);
-			pnlHero3Status.add(lblHero3Ability);
-			
-			JLabel lblHero3Health = new JLabel(String.format("Health: %d/%d", hero3.getCurrentHealth(), hero3.getMaxHealth()));
-			lblHero3Health.setFont(new Font("Tahoma", Font.PLAIN, 13));
-			lblHero3Health.setHorizontalAlignment(SwingConstants.CENTER);
-			lblHero3Health.setBounds(10, 267, 199, 44);
-			pnlHero3Status.add(lblHero3Health);
-			
-			JLabel lblHero3HealingItem = new JLabel(String.format("Healing Item:\r\n%s", hero3.getAppliedHealingItem().toString()));
-			lblHero3HealingItem.setFont(new Font("Tahoma", Font.PLAIN, 13));
-			lblHero3HealingItem.setHorizontalAlignment(SwingConstants.CENTER);
-			lblHero3HealingItem.setBounds(33, 318, 165, 32);
-			pnlHero3Status.add(lblHero3HealingItem);
-			
-			JLabel lblHero3PowerUp = new JLabel("Power Ups:");
-			lblHero3PowerUp.setFont(new Font("Tahoma", Font.PLAIN, 13));
-			lblHero3PowerUp.setHorizontalAlignment(SwingConstants.CENTER);
-			lblHero3PowerUp.setBounds(10, 361, 210, 32);
-			pnlHero3Status.add(lblHero3PowerUp);
-			
-			JTextPane txtHero3PowerUps = new JTextPane();
-			txtHero3PowerUps.setBackground(new Color(240, 240, 240));
-			for (PowerUp pu : hero3.getActivePowerUps()) {
-				txtHero3PowerUps.setText(txtHero3PowerUps.getText() + String.format("\n%s", pu.getType().toString()));
-			}
-			txtHero3PowerUps.setEditable(false);
-			txtHero3PowerUps.setBounds(10, 402, 210, 122);
-			pnlHero3Status.add(txtHero3PowerUps);
-			
-		}
-		
-		contentPanelCardLayout.show(contentPanel, MAP_PANEL_NAME);
-		btnDisplayCity.setEnabled(false);
-		add(contentPanel);
+	}
+	
+	/**
+	 * Gets the Team object associated with the current Game.
+	 * @return the Team associated with the current Game.
+	 */
+	private Team getTeam() {
+		return gameWindow.getGame().getTeam();
 	}
 }
