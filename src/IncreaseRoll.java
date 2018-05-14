e6ec14fbb90c366fce486f692bf98533b7aebcfa
Reka Norman <rekanorman@gmail.com>
2018-05-14 19:10:01 +1200

    change increase roll description

--- src/IncreaseRoll.java
+++ src/IncreaseRoll.java
@@ -13,7 +13,7 @@ public class IncreaseRoll extends PowerUp {
 	/**
 	 * A description of the power-up.
 	 */
-	private final static String DESCRIPTION = "When applied to a hero, they gain the following benefits:\nDice Roll:\nIncrease your roll by one.\nApplying multiple Increase Roll power-ups to a single hero is allowed. All Increase Roll power-ups will be consumed at the end of Dice Roll, even if they were not used.";
+	private final static String DESCRIPTION = "When playing Dice Rolls, each Increase Roll power up applied to a hero will increase their roll by one. All Increase Roll power ups applied to the hero will be consumed at the end of a game of Dice Rolls.";
 	
 	public IncreaseRoll() {
 		super(TYPE, COST, DESCRIPTION);
