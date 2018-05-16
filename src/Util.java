3c0aa3d1cffaa8d60f9487a9c959c22c66331580
Nathan Cleaver <akechta@gmail.com>
2018-05-16 20:10:56 +1200

    Util testing

--- src/Util.java
+++ src/Util.java
@@ -1,106 +1,13 @@
-import java.lang.reflect.InvocationTargetException;
 import java.util.Enumeration;
-import java.util.Scanner;
-
 import javax.swing.AbstractButton;
 import javax.swing.ButtonGroup;
 import javax.swing.JRadioButton;
 
+/**
+ * The Util class provides helpful static methods for various parts of the game.
+ * This class cannot be instantiated.
+ */
 public class Util {
-	
-	/**
-	 * Repeatedly asks the user for input using the given prompt until a valid integer
-	 * between 1 and the given maxValue inclusive is entered.
-	 * @param maxValue	The highest integer which should be accepted as valid input.
-	 * @param prompt	The prompt message which asks the user for input.
-	 * @return			The valid integer entered by the user.
-	 */
-	public static int getIntFromUser(int maxValue, String prompt) {
-		if (maxValue < 1)
-			throw new IllegalArgumentException("maxValue must be atleast 1");
-		
-		String invalidInputMessage = String.format("Please enter a number between 1 and %s.", maxValue);
-		if (maxValue == 1)
-			invalidInputMessage = "Please enter 1.";
-				
-		Scanner sc = new Scanner(System.in);
-		int number = -1;
-		boolean isValidInput = false;
-		
-		while (!isValidInput) {
-			System.out.println(prompt);
-			
-			String input = sc.nextLine();
-			try {
-				number = Integer.parseInt(input);
-			} catch (NumberFormatException e) {
-			} finally {
-				if (number >= 1 && number <= maxValue) {
-					isValidInput = true;
-				} else {
-					System.out.println(invalidInputMessage);
-				}
-			}
-		}
-
-		return number;
-	}
-	
-	/**
-	 * An overloaded version of getIntFromUser which allows a minValue to be
-	 * specified as well as a max value.
-	 * Repeatedly asks the user for input using the given prompt until a valid integer
-	 * between minValue and given maxValue inclusive is entered.
-	 * @param minValue  The lowest integer which should be accepted as valid input.
-	 * @param maxValue	The highest integer which should be accepted as valid input.
-	 * @param prompt	The prompt message which asks the user for input.
-	 * @return			The valid integer entered by the user.
-	 */
-	public static int getIntFromUser(int minValue, int maxValue, String prompt) {
-		if (maxValue < 1)
-			throw new IllegalArgumentException("maxValue must be atleast 1");
-		
-		String invalidInputMessage = String.format("Please enter a number between %s and %s.",
-														minValue, maxValue);
-		if (maxValue == minValue)
-			invalidInputMessage = String.format("Please enter %s.", minValue);
-				
-		Scanner sc = new Scanner(System.in);
-		int number = -1;
-		boolean isValidInput = false;
-		
-		while (!isValidInput) {
-			System.out.println(prompt);
-			
-			String input = sc.nextLine();
-			try {
-				number = Integer.parseInt(input);
-			} catch (NumberFormatException e) {
-			} finally {
-				if (number >= minValue && number <= maxValue) {
-					isValidInput = true;
-				} else {
-					System.out.println(invalidInputMessage);
-				}
-			}
-		}
-
-		return number;
-	}
-	
-	
-	/**
-	 * Asks the user the given question and gives them the option to choose yes
-	 * or no.
-	 * @param question	The yes or no question to ask the user.
-	 * @return	true if the user answers Yes, false if the user answers No.
-	 */
-	public static boolean getYesNo(String question) {
-		System.out.println(question);
-		System.out.println("1. Yes\n2. No\n");
-		int choice = Util.getIntFromUser(2, "Enter your choice:");
-		return choice == 1;
-	}
 	
 	/**
 	 * Takes a given class as a class object and returns a new instance of that
@@ -114,13 +21,11 @@ public class Util {
 		Object newInstance = null;
 		try {
 			newInstance = className.getConstructor().newInstance();
-		} catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException
-				| NoSuchMethodException | SecurityException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return newInstance;
 	}
-	
 	
 	/**
 	 * Takes a ButtonGroup of JRadioButtons and returns the selected button.
--- src/UtilTest.java
+++ src/UtilTest.java
@@ -1,123 +1,12 @@
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.io.ByteArrayInputStream;
+import javax.swing.ButtonGroup;
+import javax.swing.JRadioButton;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class UtilTest {
-
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterAll
-	static void tearDownAfterClass() throws Exception {
-	}
-
-	@BeforeEach
-	void setUp() throws Exception {
-	}
-
-	@AfterEach
-	void tearDown() throws Exception {
-	}
-
-	@Test
-	final void testGetIntFromUser() {
-		// Set System.in to a different input stream for testing
-		ByteArrayInputStream in = new ByteArrayInputStream("3\n".getBytes());
-		System.setIn(in);
 		
-		// Entering a number in the correct range returns that number
-		int input = Util.getIntFromUser(5, "Prompt");
-		assertEquals(3, input);
-		
-		// Entering 1 returns 1
-		in = new ByteArrayInputStream("1\n".getBytes());
-		System.setIn(in);
-		input = Util.getIntFromUser(5, "Prompt");
-		assertEquals(1, input);
-		
-		// 0 is not accepted
-		in = new ByteArrayInputStream("0\n1\n".getBytes());
-		System.setIn(in);
-		input = Util.getIntFromUser(5, "Prompt");
-		assertEquals(1, input);
-		
-		// A number greater than maxValue is not accepted
-		in = new ByteArrayInputStream("6\n1\n".getBytes());
-		System.setIn(in);
-		input = Util.getIntFromUser(5, "Prompt");
-		assertEquals(1, input);
-		
-		// A String is not accepted
-		in = new ByteArrayInputStream("string\n1\n".getBytes());
-		System.setIn(in);
-		input = Util.getIntFromUser(5, "Prompt");
-		assertEquals(1, input);
-		
-		// A decimal number is not accepted
-		in = new ByteArrayInputStream("3.5\n1\n".getBytes());
-		System.setIn(in);
-		input = Util.getIntFromUser(5, "Prompt");
-		assertEquals(1, input);
-		
-		// Repeatedly asks for input until a valid number is entered
-		in = new ByteArrayInputStream("0\n6\n2.5\n-3\nstring\nanother string\n1\n".getBytes());
-		System.setIn(in);
-		input = Util.getIntFromUser(5, "Prompt");
-		assertEquals(1, input);
-		
-		// An exception is thrown if maxValue is less than 1
-		boolean exceptionThrown = false;
-		try {
-			in = new ByteArrayInputStream("0\n".getBytes());
-			System.setIn(in);
-			input = Util.getIntFromUser(0, "Prompt");
-		} catch (IllegalArgumentException iae) {
-			exceptionThrown = true;
-		}
-		assertTrue(exceptionThrown);
-		
-		// maxValue can be 1
-		exceptionThrown = false;
-		input = -1;
-		try {
-			in = new ByteArrayInputStream("1\n".getBytes());
-			System.setIn(in);
-			input = Util.getIntFromUser(1, "Prompt");
-		} catch (IllegalArgumentException iae) {
-			exceptionThrown = true;
-		}
-		assertFalse(exceptionThrown);
-		assertEquals(1, input);
-		
-		// Restore System.in
-		System.setIn(System.in);
-	}
-	
-	@Test
-	final void testGetYesNo() {
-		// Entering 1 returns true
-		ByteArrayInputStream in = new ByteArrayInputStream("1\n".getBytes());
-		System.setIn(in);
-		assertTrue(Util.getYesNo("Question"));
-		
-		// Entering 2 returns false
-		in = new ByteArrayInputStream("2\n".getBytes());
-		System.setIn(in);
-		assertFalse(Util.getYesNo("Question"));
-		
-		// Repeatedly asks for input until the input is valid
-		in = new ByteArrayInputStream("23\nstring\n0\n1.2\n1\n".getBytes());
-		System.setIn(in);
-		assertTrue(Util.getYesNo("Question"));
-	}
-	
 	@Test
 	final void testInstantiate() {
 		// Create a new villain
@@ -135,7 +24,28 @@ class UtilTest {
 		assertEquals(PowerUpType.TIEBREAKER, powerUp.getType());
 	}
 
-	
+	@Test
+	final void testSelectedButton() {
+		ButtonGroup bg = new ButtonGroup();
+		
+		JRadioButton b1 = new JRadioButton();
+		JRadioButton b2 = new JRadioButton();
+		
+		bg.add(b1);
+		bg.add(b2);
+		
+		// Check that selectedButton returns b1 as expected
+		b1.setSelected(true);
+		assertEquals(Util.selectedButton(bg), b1);
+		
+		/* Check that when a new button is selected, b1
+		* becomes unselected and selectedButton returns
+		* the new button
+		*/
+		b2.setSelected(true);
+		assertNotEquals(b1.isSelected(), true);
+		assertEquals(Util.selectedButton(bg), b2);
+	}
 	
 	
 	
